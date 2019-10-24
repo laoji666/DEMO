5cd4e906f2ae277b2ba4b6048e0668deded73e94
laoji <1026165407@qq.com>
2019-10-24 18:47:35 +0800

    新增ssm整合shiro

---
+++ shiro_ssm/src/main/java/com/laoji/demo/service/UsersService.java
@@ -0,0 +1,29 @@
+package com.laoji.demo.service;
+
+import com.laoji.demo.pojo.Users;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Set;
+
+public interface UsersService {
+
+
+    /**
+     * 根据用户名查询用户对象是否存在。
+     * @param username
+     * @return Users
+     */
+    Users selectByUsername(String username);
+
+    /**
+     * 根据用户名查询所有的角色信息
+     * @param username
+     * @return
+     */
+    Set<String> selectRnamesByUserName(String username);
+
+    /**
+     * 查询所有角色的方法
+     */
+    Set<String> selectRolesAllRnames();
+}
